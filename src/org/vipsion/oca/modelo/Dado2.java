735c4b80ee53e11f73fa42688d2466d832300ef9
Alejandro Monedero <alejandro.monedero.acebal@gmail.com>
2019-04-03 22:25:18 +0200

    Segunda instantánea creando Dado2

---
+++ src/org/vipsion/oca/modelo/Dado2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vipsion.oca.modelo;
+
+/**
+ *
+ * @author alejandromonederoacebal
+ */
+public class Dado2 extends Casilla {
+    
+    public Dado2(int posicion) {
+        super(posicion);
+    }
+    
+    @Override
+    public void posaFicha(Ficha ficha){
+        
+    }
+    
+}
